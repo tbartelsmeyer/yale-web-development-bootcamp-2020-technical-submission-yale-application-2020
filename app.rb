e2c882fc3b47a0d69962752d4e2db55252c27854
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-07 23:06:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
 
+name = 'Tilman'
+age = 23
+favorite_food = "Fried Morel Mushrooms"
+height_in_inches = 73
+height_message = height_in_inches > AVERAGE_HEIGHT_IN_INCHES ? "You are taller than average." : "You are not taller than average."
+
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
