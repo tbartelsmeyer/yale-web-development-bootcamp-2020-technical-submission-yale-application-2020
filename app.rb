efbac034c47df108170243e45e9c5f04085c01f8
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-07 23:01:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ name = 'Tilman'
 age = 23
 favorite_food = "Fried Morel Mushrooms"
 height_in_inches = 73
+"You are taller than average." Otherwise, it should say "You are not taller than average."
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
