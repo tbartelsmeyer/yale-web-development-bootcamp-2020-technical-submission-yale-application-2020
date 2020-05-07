df05dbb6fd5d2a489befca6325676e7f57acbe4b
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-07 22:46:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
-
+name = "Tilman"
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
