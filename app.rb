a1d43e72228bb6f3e4557f62b2d9f9ac4b015ec3
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-07 22:48:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 name = 'Tilman'
 age = 23
 favorite_food = "Fried Morel Mushrooms"
+height_in_inches = 73
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
